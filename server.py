9fbb243a57c866da14bc0b04c18fc60060d9b431
ruf <ruf@knowit.no>
2017-02-22 09:00:45 +0100

    Allow all methods + fwd return code

--- server.py
+++ server.py
@@ -6,12 +6,24 @@ import requests
 
 app = flask.Flask(__name__)
 
+method_requests_mapping = {
+    'GET': requests.get,
+    'HEAD': requests.head,
+    'POST': requests.post,
+    'PUT': requests.put,
+    'DELETE': requests.delete,
+    'PATCH': requests.patch,
+    'OPTIONS': requests.options,
+}
 
-@app.route('/<path:url>')
+
+@app.route('/<path:url>', methods=method_requests_mapping.keys())
 def proxy(url):
-    request = requests.get(url, stream=True, data=flask.request.args)
+    requests_function = method_requests_mapping[flask.request.method]
+    request = requests_function(url, stream=True, data=flask.request.args)
     response = flask.Response(flask.stream_with_context(request.iter_content()),
-                              content_type=request.headers['content-type'])
+                              content_type=request.headers['content-type'],
+                              status=request.status_code)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
