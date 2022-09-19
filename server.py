2b2e7eb51b5863fa22d1bd9d81a2920446200bc3
sehnryr <youn@melois.dev>
2022-09-19 20:08:58 +0200

    apply black uncompromising formatting

--- server.py
+++ server.py
@@ -7,27 +7,29 @@ import requests
 app = flask.Flask(__name__)
 
 method_requests_mapping = {
-    'GET': requests.get,
-    'HEAD': requests.head,
-    'POST': requests.post,
-    'PUT': requests.put,
-    'DELETE': requests.delete,
-    'PATCH': requests.patch,
-    'OPTIONS': requests.options,
+    "GET": requests.get,
+    "HEAD": requests.head,
+    "POST": requests.post,
+    "PUT": requests.put,
+    "DELETE": requests.delete,
+    "PATCH": requests.patch,
+    "OPTIONS": requests.options,
 }
 
 
-@app.route('/<path:url>', methods=method_requests_mapping.keys())
+@app.route("/<path:url>", methods=method_requests_mapping.keys())
 def proxy(url):
     requests_function = method_requests_mapping[flask.request.method]
     request = requests_function(url, stream=True, params=flask.request.args)
-    response = flask.Response(flask.stream_with_context(request.iter_content()),
-                              content_type=request.headers['content-type'],
-                              status=request.status_code)
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response = flask.Response(
+        flask.stream_with_context(request.iter_content()),
+        content_type=request.headers["content-type"],
+        status=request.status_code,
+    )
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run()
